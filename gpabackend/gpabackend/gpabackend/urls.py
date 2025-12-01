191f71cd94fb9658f604a5f6b6fc8ead80976b1b
shihas-36 <shihashihas36@gmail.com>
2025-12-01 20:32:36 +0530

    fix: remove simplejwt imports from urls.py, use custom LoginView for /token/ endpoint

--- gpabackend/gpabackend/gpabackend/urls.py
+++ gpabackend/gpabackend/gpabackend/urls.py
@@ -1,15 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from accounts.views import LoginView  # Use your custom LoginView instead of simplejwt
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('calculator.urls')),  # Include the calculator app URLs
     path('', include('accounts.urls')),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', LoginView.as_view(), name='token_obtain_pair'),  # Use custom LoginView
 ]
